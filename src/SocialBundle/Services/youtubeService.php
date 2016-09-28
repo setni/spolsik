9957204efc7527dca27be85c76242ce9a2853141
DBNA <dbna@iMac-de-DBNA.local>
2016-09-28 16:58:14 +0200

    V3

---
+++ src/SocialBundle/Services/youtubeService.php
@@ -0,0 +1,28 @@
+<?php
+
+// src/SocialBundle/Services/youtubeService.php
+
+
+namespace SocialBundle\youtubeService;
+
+
+class youtubeService
+
+{
+    private query;
+    
+    public function __construct ()
+    {
+        
+    }
+    
+    public function search ($query)
+    {
+        return $query;
+    }
+    
+    private function getAPIResult()
+    {
+        
+    }
+}
